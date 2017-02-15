e7721e64d949e1c021c263dcd3ae6476b178a269
Omar Zorgui <Omar@Omars-MacBook-Pro.local>
2017-02-15 03:19:19 +0100

    Added sliders, made the server ready (making use of hbs)

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -13,5 +13,47 @@ console.clear();
     });
   };
 
-  elementDOM.addEventListener('click', showBOXModel);
+//   elementDOM.addEventListener('click', showBOXModel);
+
+
+  var readMoreButton = document.querySelector('#home-text-button');
+    if(readMoreButton) {
+        readMoreButton.addEventListener('click', showMoreText);
+    }
+  
+
+  function showMoreText() {
+      
+      var textArea = document.querySelector('#home-text');
+      textArea.style.display = "block";
+      readMoreButton.style.display = "none";
+
+  }
+
+
 }());
+
+$(document).ready(function() {
+    $("#lightSlider").lightSlider({
+        item: 1,
+        gallery: false,
+        pager: false,
+        useCSS: false,
+        auto: true,
+        loop: true
+    }); 
+
+    $('#recensionSlider').lightSlider({
+        item: 2,
+        vertical: true,
+        adaptiveHeight: true,
+        gallery: false,
+        pager: false,
+        verticalHeight: 240,
+        auto: true,
+        loop: true
+    })
+
+
+
+});
--- src/server.js
+++ src/server.js
@@ -1,9 +1,81 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('logger');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+var hbs = require('hbs');
 
-const app = express();
+var app = express();
 
-app.use(express.static(path.join('__DIRNAME', '..')));
+// Comment this out when deploying
+app.disable('view cache');
+
+hbs.registerPartial('footer-extra-text', fs.readFileSync(path.join(__dirname, '../views/partials/footer-extra-text.hbs'), 'utf8'))
+hbs.registerPartial('footer-normal', fs.readFileSync(path.join(__dirname, '../views/partials/footer-normal.hbs'), 'utf8'))
+
+app.set('views', path.join(__dirname, '../views'));
+app.engine('hbs', hbs.__express)
+app.set('view engine', 'hbs');
+
+/* Below are all the routes */
+
+app.get('/', (req, res, next) => {
+    res.render('index', { title: 'Buitenbos Customs' });
+})
+
+app.get('/index.html', (req, res, next) => {
+    res.render('index', { title: 'Buitenbos Customs' });
+})
+
+app.get('/prijzen.html', (req, res, next) => {
+    res.render('prijzen', { title: 'Buitenbos Customs' });
+})
+
+app.get('/afspraak.html', (req, res, next) => {
+    res.render('afspraak', { title: 'Buitenbos Customs' });
+})
+
+app.get('/offerte.html', (req, res, next) => {
+    res.render('offerte', { title: 'Buitenbos Customs' });
+})
+
+app.get('/contact.html', (req, res, next) => {
+    res.render('contact', { title: 'Buitenbos Customs' });
+})
+
+app.get('/bedankt.html', (req, res, next) => {
+    res.render('bedankt', { title: 'Buitenbos Customs' });
+})
+
+/* end of all the routes */
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
 
 const port = process.env.PORT || 3000;
 
